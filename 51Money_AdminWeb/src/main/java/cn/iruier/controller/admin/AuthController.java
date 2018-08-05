8f63de629cc25eeed18b1d5a020e0435c8480c99
iruier <785856696@qq.com>
2018-08-05 17:15:05 +0800

    2018.8.5---账户开通、信用额度审批

--- 51Money_AdminWeb/src/main/java/cn/iruier/controller/admin/AuthController.java
+++ 51Money_AdminWeb/src/main/java/cn/iruier/controller/admin/AuthController.java
@@ -3,12 +3,18 @@ package cn.iruier.controller.admin;
 import cn.iruier.core.vo.PageVo;
 import cn.iruier.core.vo.ResultVo;
 import cn.iruier.core.vo.UserRiskVo;
+import cn.iruier.entity.user.Account;
+import cn.iruier.entity.user.Risk;
 import cn.iruier.entity.user.UserInfo;
+import cn.iruier.mapper.user.RiskMapper;
 import cn.iruier.service.admin.AuthService;
+import cn.iruier.service.user.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Author: iruier
@@ -21,6 +27,9 @@ public class AuthController {
     @Autowired
     private AuthService service;
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping("/list")
     public PageVo<UserInfo> queryByPage(int page, int limit) {
         return service.queryByPage(page, limit);
@@ -28,8 +37,18 @@ public class AuthController {
 
     @GetMapping("/update")
     public ResultVo update(int user_id, int status) {
-
-        return service.update(user_id, status);
+        ResultVo resultVo = service.update(user_id, status);
+        if (resultVo.getCode() == 0) {
+            Account account = new Account();
+            account.setUser_id(user_id);
+            account.setTotalMoney(0);
+            account.setRedPackage(0);
+            account.setForceMoney(0);
+            account.setCarditMoney(0);
+            account.setStatus(1);
+            accountService.insert(account);
+        }
+        return resultVo;
     }
 
     @GetMapping("/riskList")
@@ -39,6 +58,31 @@ public class AuthController {
 
     @GetMapping("/updateRiskStatus")
     public ResultVo updateRisk(int user_id, int status) {
+        List<Risk> risks = service.queryByUid(user_id);
+        Account account = new Account();
+        int totalMoney = 0;
+        for (Risk risk : risks) {
+            switch (risk.getType()) {
+                case 1:
+                    totalMoney = totalMoney + risk.getScore() * 500;
+                    break;
+                case 2:
+                    totalMoney = totalMoney + risk.getScore() * 400;
+                    break;
+                case 3:
+                    totalMoney = totalMoney + risk.getScore() * 300;
+                    break;
+                case 4:
+                    totalMoney = totalMoney + risk.getScore() * 200;
+                    break;
+                case 5:
+                    totalMoney = totalMoney + risk.getScore() * 100;
+                    break;
+            }
+            account.setUser_id(user_id);
+            account.setTotalMoney(totalMoney);
+            accountService.update(account);
+        }
         return service.updateRiskStatus(user_id, status);
     }
 }
